3aa88816e943ed70c1da9acd3a5b3e1790c74667
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-12-11 10:10:59 +0500

    feat

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,12 +9,23 @@ import "firebase/firestore"
 const Home = () => {
   return (
     <div id="homeMainBody">
-      <div className='container'>
+      <div className='container_own'>
         <div className="row">
-          <div className="col-md-3 border">
-
+          <div className="col-md-3 border h-100">
+            <div className="filter_div">
+              <div className="container">
+                <div className="container">
+                  <div className="row">
+                    <h6 className="filter_txt">
+                      Filters
+                    </h6>
+                    <p className='border_line'></p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="col-md-9 border">
+          <div className="col-md-9 border h-100">
 
           </div>
         </div>
